eba04739eb49157c600a6bbb4e4c52d19f9dbfbc
Anjali Patil1413 <anjalip@mobifyi.com>
2020-12-18 15:10:40 +0530

    Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,32 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Bhutta Kiss ',
-      'A super-tasty Kiss - just awesome!',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5zlyGhPsL2owm2yVS16L3NvEJh0_z0VSwFg&usqp=CAU',
-      [
-        new Ingredient('Corn pinuts', 1),
-        new Ingredient('Greted corn stir fry', 20)
-      ]),
-    new Recipe('Shahi Paneer ',
-      'A super-tasty curry - just awesome!',
-      'https://i1.wp.com/alltimehungry.com/wp-content/uploads/2020/06/Shahi-Paneer-All-Time-Hunrgy.jpg?resize=795%2C481&ssl=1',
-      [
-        new Ingredient('Matter', 2),
-        new Ingredient('Franch Rice', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Bhutta Kiss ',
+  //     'A super-tasty Kiss - just awesome!',
+  //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5zlyGhPsL2owm2yVS16L3NvEJh0_z0VSwFg&usqp=CAU',
+  //     [
+  //       new Ingredient('Corn pinuts', 1),
+  //       new Ingredient('Greted corn stir fry', 20)
+  //     ]),
+  //   new Recipe('Shahi Paneer ',
+  //     'A super-tasty curry - just awesome!',
+  //     'https://i1.wp.com/alltimehungry.com/wp-content/uploads/2020/06/Shahi-Paneer-All-Time-Hunrgy.jpg?resize=795%2C481&ssl=1',
+  //     [
+  //       new Ingredient('Matter', 2),
+  //       new Ingredient('Franch Rice', 1)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] =[];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
--- src/app/shared/data.storage.service.ts
+++ src/app/shared/data.storage.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 
@@ -12,19 +14,31 @@ export class DataStorageService {
     storeRecipes(){
         const recipes = this.recipeService.getRecipes()
         this.http.put('https://course-project-279910-default-rtdb.firebaseio.com/recipes.json',recipes)
-                .subscribe(
-                    (response)=>{
-                        console.log(response)
-                    }
-                )
+            .subscribe(
+                (response)=>{
+                    console.log(response)
+                }
+            )
     }
 
     fetchRecipes(){
-        this.http.get('https://course-project-279910-default-rtdb.firebaseio.com/recipes.json')
-        .subscribe(
-            (recipes)=>{
-                console.log(recipes);
-            }
-        )
+        this.http.get<Recipe[]>(
+            'https://course-project-279910-default-rtdb.firebaseio.com/recipes.json'
+            )
+            .pipe(
+                map(recipes=>{
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients ? recipe.ingredients : []
+                        };
+                    });
+                })
+            )
+            .subscribe(
+                (recipes)=>{
+                    this.recipeService.setRecipes(recipes);
+                }
+            )
     }
 }
